0e13b647b7ad140a4e1d35a06139db6854dd965a
Taylor Schmidt <taylor9723@gmail.com>
2022-12-15 15:56:38 +1100

    fixed scroll issue - settimeout fix

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, ViewportScroller } from '@angular/common';
 import { LayoutComponent } from './layout.component';
-import { RouterModule } from '@angular/router';
+import { NavigationEnd, Router, RouterModule, Scroll } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { routes } from '../app-routing.module';
+import { filter } from 'rxjs';
 
 
 
@@ -16,15 +17,36 @@ import { routes } from '../app-routing.module';
   ],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes, {
-      scrollPositionRestoration: 'enabled',
-      anchorScrolling: 'enabled',
-      scrollOffset: [0, 60]
-    })
+    RouterModule.forRoot(routes)
   ],
   exports: [
     LayoutComponent,
     RouterModule
   ]
 })
-export class LayoutModule { }
+export class LayoutModule {
+  constructor(private router: Router, private viewportScroller: ViewportScroller) {
+    this.scrollToAnchor();
+  }
+
+  scrollToAnchor() {
+    this.viewportScroller.setOffset([0, 60]);
+    this.router.events.pipe(filter(e => e instanceof NavigationEnd)).subscribe((event) => {
+      const navEnd = event as unknown as NavigationEnd;
+      const url = navEnd.urlAfterRedirects;
+      if (url === '/') {
+        setTimeout(() => {
+          this.viewportScroller.scrollToPosition([0, 0]);
+        }, 300);
+        return;
+      }
+
+      const anchorToScroll = this.router.parseUrl(navEnd.urlAfterRedirects).fragment;
+      if (anchorToScroll) {
+        setTimeout(() => {
+          this.viewportScroller.scrollToAnchor(anchorToScroll);
+        }, 300);
+      }
+    });
+  }
+}
